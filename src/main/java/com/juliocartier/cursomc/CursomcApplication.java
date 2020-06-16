419204c4686c8d58ae3b4bfef48e06027e0e0569
Julio Cartier <juliocartier@gmail.com>
2020-06-15 22:11:46 -0300

    Foi feito o estado e a cidade como também os repository

--- src/main/java/com/juliocartier/cursomc/CursomcApplication.java
+++ src/main/java/com/juliocartier/cursomc/CursomcApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.juliocartier.cursomc.domain.Categoria;
+import com.juliocartier.cursomc.domain.Cidade;
+import com.juliocartier.cursomc.domain.Estado;
 import com.juliocartier.cursomc.domain.Produto;
 import com.juliocartier.cursomc.repositorys.CategoriaRepository;
+import com.juliocartier.cursomc.repositorys.CidadeRepository;
+import com.juliocartier.cursomc.repositorys.EstadoRepository;
 import com.juliocartier.cursomc.repositorys.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,8 +52,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Uberlandia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 	}
 	
 	
---
+++ src/main/java/com/juliocartier/cursomc/repositorys/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.juliocartier.cursomc.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juliocartier.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+	
+	
+}
